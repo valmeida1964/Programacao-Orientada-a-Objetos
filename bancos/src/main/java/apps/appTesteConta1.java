335b4eccf21136bab79aaca487dedfc2a7e4fb7c
vagner almeida <valmeida1964@hotmail.com>
2025-03-30 10:05:15 -0300

    aula 16 - tratamento de excecoes em aplicacao Java

--- bancos/src/main/java/apps/appTesteConta1.java
+++ bancos/src/main/java/apps/appTesteConta1.java
@@ -4,6 +4,8 @@ package apps;
 import classes.Pessoa;
 import contas.Conta;
 import contas.ContaComum;
+import java.util.InputMismatchException;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class appTesteConta1 {
@@ -11,13 +13,20 @@ public class appTesteConta1 {
     public static void main(String[] args) {
 
         Scanner teclado = new Scanner(System.in);
+        teclado.useLocale(Locale.FRENCH);
         
-        int numero;
+        int numero = 0;
         String nome, email;
-        float saldo, valordeposito;
+        float saldo = 0, valordeposito = 0;
         
         System.out.print("Numero da conta .....: ");
+        try{
         numero = teclado.nextInt();
+        }
+        catch(InputMismatchException e){
+            System.out.println("Informe um valor numerico, valido, para o numero da conta, ex:123456");
+            System.exit(0);
+        }
         
         System.out.print("Correntista .........: ");
         nome = teclado.next();
@@ -26,16 +35,28 @@ public class appTesteConta1 {
         email = teclado.next();
         
         System.out.print("Saldo Inicial .......: ");
-        saldo = teclado.nextFloat();
+        try{
+            saldo = teclado.nextFloat();
+        }
+        catch(InputMismatchException e){
+            System.out.println("Informe um valor monetario, valido, para o saldo inicial ex:1234,56");
+            System.exit(0);
+        }
         
         Conta contacomum = new ContaComum(numero, new Pessoa(nome, email), saldo);
         
         System.out.print("Valor do deposito ...: ");
-        valordeposito = teclado.nextFloat();
+        try{
+            valordeposito = teclado.nextFloat();
+        }
+        catch(InputMismatchException e){
+            System.out.println("Informe um valor monetario, valido, para o valor de deposito ex:1234,56");
+            System.exit(0);
+        }
         
         contacomum.depositar(valordeposito);
         
-        System.out.print("Saldo atual .........: " + contacomum.getSaldo());
+        System.out.printf("Saldo atual .........: %.2f\n ", contacomum.getSaldo());
     }
     
 }
